11bfc8451757309f6b4591cabc871b39824bf653
Steinburglar <steinbergerlucas02@gmail.com>
2025-04-16 17:56:38 -0400

    added comparisonwith model predictions by area

--- positron/Analysis.py
+++ positron/Analysis.py
@@ -1,22 +1,63 @@
 import pandas as pd
 import math
 import numpy as np
+from positron.Functions import *
 from cavendish.utils.Functions import *
 from compton.Functions import *
 from millikan.functions import *
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-def plot_counts(counts):
+def plot_counts(df, Normalized = False, overlap=False):
     """Plots the counts as a function of angle.
 
     Args:
-        counts (dict): dictionary of angle (int): counts (int) pairs.
+        df (pd.DataFrame): dataframe with index as "angle" and columns "counts" and "error".
     """
-    angles = list(counts.keys())
-    counts = list(counts.values())
-    plt.errorbar(angles, counts, yerr=np.sqrt(counts), marker = "o")
+    angles = df.index
+    counts = df["Counts"]
+    errors = df["Error"]
+    if Normalized:
+        counts = df["normalized_counts"]
+        errors = df["normalized_error"]
+    plt.errorbar(angles, counts, yerr=errors, marker="o")
     plt.xlabel("Angle (degrees)")
     plt.ylabel("Counts")
     plt.title("Counts vs Angle")
+    
+    if overlap:
+        #plot the overlap
+        x = np.linspace(-6, 6, 100)
+        overlap = [normalized_overlap(angle) for angle in x]
+        plt.plot(x, overlap, label="Overlap", color="red")
+        plt.legend()
+    
     plt.show()
+    
+    
+
+def normalize_counts(df):
+    """Normalizes the counts in a DataFrame to the maximum count.
+
+    Args:
+        df (pd.DataFrame): dataframe with columns "angle" and "counts".
+
+    Returns:
+        pd.DataFrame: input dataframe with new collumn of normalized counts, and normalized errors
+    """
+    max_count = df["Counts"].max()
+    print(max_count)
+    df["normalized_counts"] = df["Counts"] / max_count
+    df["normalized_error"] = df["Error"] / max_count
+    return df
+
+
+def total_counts(dfs):
+    #takes in a dictionary of dataframes for each angle, returns a dataframe with the total counts and errors at each angle
+    counts = {angle: df["Counts"].sum() for angle, df in dfs.items()} #create a dictionary from the dfs
+    counts = pd.DataFrame.from_dict(counts, orient='index', columns=["Counts"]) # create a dataframe from the dictionary
+    counts.index.name = "Angle" # set the index name to "Angle"
+
+    counts["Error"] = np.sqrt(counts["Counts"])
+    counts = normalize_counts(counts)
+    return counts
---
+++ positron/Functions.py
@@ -0,0 +1,45 @@
+"""contains functions for the positron analysis
+"""
+import numpy as np
+import math
+
+
+def circles_overlap(angle, r1, r2, length):
+    """returns the overlapping area of two circles of radius r1 and r2, wth a distance d between their centers
+    """
+    d = d_from_angle(angle, length)
+    if d>= r1 + r2:
+        print(f"No overlap at {angle} degrees")
+        return 0
+    elif d <= np.abs(r1 - r2):
+        print(f"One circle is inside the other at {angle} degrees")
+        return math.pi * min(r1, r2)**2
+    else:
+        d1 = (r1**2 - r2**2 + d**2)/(2*d)
+        d2 = d - d1
+        term1 = r1**2 * math.acos(d1/r1)
+        term2 = r2**2 * math.acos(d2/r2)
+        term3 = d1 * math.sqrt(r1**2 - d1**2)
+        term4 = d2 * math.sqrt(r2**2 - d2**2)
+        area = term1 + term2 - term3 - term4
+        print(f"Overlap at {angle} degrees: {area}")
+        return area
+
+def d_from_angle(angle, length):
+    """returns the distance between centers of apperature circles when projected on a flat plane
+
+    Args:
+        angle (_float_): angle in degrees of arm
+        length (_type_): true length from pivot to apperature, i.e not to opening of columnator but to the detector itself
+    """
+    radians = np.abs(math.radians(angle))
+    d = length * math.tan(radians)
+    return d
+
+def normalized_overlap(angle, r1=1.75, r2=0.6, length=30):
+    """returns the normalized overlapping area of two circles of radius r1 and r2, wth a distance d between their centers
+    """
+    area = circles_overlap(angle, r1, r2, length)
+    norm_area = area / (math.pi * min(r1, r2)**2)
+    return norm_area
+
